7cc42e348cabc125aeabb86dfa407136a366664c
iestmgit <radmehr3325@chmail.ir>
2026-06-15 07:02:49 +0330

    Update server.js

--- server.js
+++ server.js
@@ -89,5 +89,5 @@ app.get("/api/prices", async (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log("server running http://localhost:3000")
-})
+  console.log(`Server is running on port ${PORT}`);
+});
